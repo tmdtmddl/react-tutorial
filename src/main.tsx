020edf3a45ce86601a18802948bfe8abfd109caf
tmdtmddl <tmddlqkqh@naver.com>
2025-02-27 16:32:23 +0900

    dlf

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import StudentApp from "./StudentApp";
 import "./index.css";
-import RConainer from "./r/Rconainer";
 import App from "./App";
-// import App from './App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <StudentApp /> */}
-    <RConainer />
-    {/* <App /> */}
+    <App />
   </StrictMode>
 );
