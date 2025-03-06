89baa07e057abfd04acd73e4dd36f416fbc63d5c
tmdtmddl <tmddlqkqh@naver.com>
2025-03-07 08:56:36 +0900

    icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,15 @@
 import { Button, Container, Typo } from "../components";
+import { TiHeart } from "react-icons/ti";
 
 const Home = () => {
   return (
     <Container.Row className="w-full h-screen justify-center items-center">
       <Container.Col className="gap-y-5 max-w-90 p-5">
-        <Typo.H1 className="text-center">
+        <Typo.H1 className="text-center relative">
           나만의 사랑을 찾고 커플이 되어 지옥같은 현실에서 탈출하세요.
+          <TiHeart className="absolute bottom-25 left-75 rotate-30 text-pink-500 " />
         </Typo.H1>
+
         <Button.Link href={"signup"} className="bg-pink-400 text-white">
           탈출하기
         </Button.Link>
